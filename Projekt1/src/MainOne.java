2eea3697e33e2b60549b2f7021542477013d7e23
William <william.geismar@gmail.com>
2017-03-08 10:40:26 +0100

    test

---
+++ Projekt1/src/MainOne.java
@@ -0,0 +1,9 @@
+/**
+ * Created by William on 08-03-2017.
+ */
+public class MainOne {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ Session7/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by William on 08-03-2017.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
