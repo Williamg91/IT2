f9eab7dbd193b2570d5340abbc03c5abbbf53314
Williamg91 <william.geismar@gmail.com>
2017-03-25 13:54:57 +0100

    Tilføjet JFreechart bibliotek til at lave grafer nemt.

---
+++ Projekt1/src/Grafpanel.java
@@ -0,0 +1,5 @@
+/**
+ * Created by William on 3/25/2017.
+ */
+public class Grafpanel {
+}
--- Projekt1/src/MainOne.java
+++ Projekt1/src/MainOne.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
-
+import org.jfree.chart.*;
+import org.jfree.data.*;
+import org.jfree.ui.*;
 /**
  * Created by William on 08-03-2017.
  */
@@ -10,13 +12,19 @@ public class MainOne {
 //laver ramme som jeg fylder med et Panel.
 
 
-        Control cont = new Control(new GUI());
+        //Control cont = new Control(new GUI());
 
 
     }
 
     static void setupFrame() {
-       // GUI ramme = new GUI();
+       JFrame ramme = new JFrame("grafpanel");
+
+        ramme.setVisible(true);
+        ramme.setSize(700, 400);
+        ramme.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        ramme.setLocation(200, 300);
 
 
 
